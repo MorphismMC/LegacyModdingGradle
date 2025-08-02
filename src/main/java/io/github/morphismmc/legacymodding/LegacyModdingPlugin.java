875d76ee7ac4707cf59295843a4c15b5486f5b2f
Gate Guardian <gateguardian@qq.com>
2025-08-02 22:46:14 +0800

    Apply Java Library Plugin

--- src/main/java/io/github/morphismmc/legacymodding/LegacyModdingPlugin.java
+++ src/main/java/io/github/morphismmc/legacymodding/LegacyModdingPlugin.java
@@ -2,11 +2,12 @@ package io.github.morphismmc.legacymodding;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaLibraryPlugin;
 
 public class LegacyModdingPlugin implements Plugin<Project> {
 
     @Override
-    public void apply(Project target) {
-
+    public void apply(Project project) {
+        project.getPlugins().apply(JavaLibraryPlugin.class);
     }
 }
